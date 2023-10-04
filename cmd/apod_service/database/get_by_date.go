6263ce49a97edd0edc04eabe6c45aaaf2568bbbc
Ruslan Kolegov <ruslan@fins.dev>
2023-10-05 01:34:38 +0600

    remove log

--- cmd/apod_service/database/get_by_date.go
+++ cmd/apod_service/database/get_by_date.go
@@ -3,9 +3,8 @@ package database
 import (
 	"context"
 	"database/sql"
-	"log"
 
-	"github.com/Marsredskies/apod_service/cmd/apod_service"
+	apod "github.com/Marsredskies/apod_service/cmd/apod_service"
 )
 
 func (r *DB) GetByDate(ctx context.Context, date string) (*apod.ImageData, error) {
@@ -13,7 +12,6 @@ func (r *DB) GetByDate(ctx context.Context, date string) (*apod.ImageData, error
 
 	err := r.db.GetContext(ctx, &img, GET_BY_DATE, date)
 	if err == sql.ErrNoRows {
-		log.Println("no rows")
 		return nil, nil
 	}
 
