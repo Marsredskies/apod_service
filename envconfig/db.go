7c41ccb0ee417948f7e0ec971461f3262881703d
Ruslan Kolegov <ruslan@fins.dev>
2023-10-05 02:31:16 +0600

    update default env

--- envconfig/db.go
+++ envconfig/db.go
@@ -3,7 +3,7 @@ package envconfig
 type Database struct {
 	Username string `envconfig:"APOD_PG_USERNAME" default:"postgres"`
 	Password string `envconfig:"APOD_PG_PASSWORD" default:"postgres"`
-	Host     string `envconfig:"APOD_PG_HOST" default:"postgres"`
+	Host     string `envconfig:"APOD_PG_HOST" default:"0.0.0.0"`
 	Port     int    `envconfig:"APOD_PG_PORT" default:"5432"`
 	DBName   string `envconfig:"APOD_PG_DATABASE_NAME" default:"postgres"`
 }
