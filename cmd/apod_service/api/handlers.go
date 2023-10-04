d083118660bdee7f48f8ed41f2e1cced99b30a6d
Ruslan Kolegov <ruslan@fins.dev>
2023-10-05 01:33:59 +0600

    update handlers

--- cmd/apod_service/api/handlers.go
+++ cmd/apod_service/api/handlers.go
@@ -1,11 +1,9 @@
 package api
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 	"regexp"
-	"text/template"
 
 	"github.com/labstack/echo"
 )
@@ -13,29 +11,30 @@ import (
 func (a *API) getAllImagesInfo(c echo.Context) error {
 	imagesData, err := a.db.GetAll(c.Request().Context())
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, fmt.Errorf("internal error: %v", err))
+		return c.JSON(http.StatusInternalServerError, 
+			fmt.Sprintf("internal error: %s", err.Error()))
 	}
 
 	if imagesData == nil {
-		return c.JSON(http.StatusNotFound, echo.Map{"error": errors.New("no images found")})
+		return c.JSON(http.StatusNotFound, echo.Map{"error": "no images found"})
 	}
 
 	return c.HTML(http.StatusOK, generateHTMLTable(imagesData))
 }
-
-var tmpl *template.Template
 
 func (a *API) getImageByDate(c echo.Context) error {
 	date := c.FormValue("date")
 	re := regexp.MustCompile(`\d{4}-\d{2}-\d{2}`)
 
 	if !re.MatchString(date) {
-		return c.JSON(http.StatusBadRequest, echo.Map{"error": "invalid data format, use YYYY-MM-YY"})
+		return c.JSON(http.StatusBadRequest,
+			echo.Map{"error": "invalid data format, use YYYY-MM-YY"})
 	}
 
 	imageData, err := a.db.GetByDate(c.Request().Context(), date)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, echo.Map{"error": fmt.Errorf("internal error: %v", err)})
+		return c.JSON(http.StatusInternalServerError,
+			echo.Map{"error": fmt.Sprintf("internal error: %v", err.Error())})
 	}
 
 	if imageData == nil {
